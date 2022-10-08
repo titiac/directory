fab90709b84dd6756581377e5ae762cc57201fba
Max <86398378+titiac@users.noreply.github.com>
2022-10-08 16:06:23 +0800

    初始化前端项目

--- directory_backend/src/main/java/com/example/directory/controller/TestController.java
+++ directory_backend/src/main/java/com/example/directory/controller/TestController.java
@@ -4,6 +4,7 @@ package com.example.directory.controller;
 import com.example.directory.bean.Test;
 import com.example.directory.service.impl.TestServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
