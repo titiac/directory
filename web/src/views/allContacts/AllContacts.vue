cba4fa8674b72745bae2237dd6f76b662776f0bb
Max <2902691270@qq.com>
2022-10-22 00:15:59 +0800

    前端开个小头，明天再完善吧

--- web/src/views/allContacts/AllContacts.vue
+++ web/src/views/allContacts/AllContacts.vue
@@ -1,17 +1,21 @@
 <template>
-    <ContentField>
-        所有联系人
-    </ContentField>
+    <div class="container">
+        <div class="row">
+            <div class="col-12"> <!-- 一个页面中占三份，共十二份 -->
+                <div class="card" style="margin-top:20px">
+                    <div class="card-body">
 
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <script>
-import ContentField from '../../components/ContentField.vue'
 
 export default {
-    components: {
-        ContentField
-    }
+ 
 }
 </script>
 
